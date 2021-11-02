97099efe956eda1a6d8f85394870dc9aa359ba10
alizokaei <ali.zokaei.1367@gmail.com>
2021-11-02 19:13:22 +0330

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,167 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="highchart">
+    <div>
+      <div>
+        <highcharts
+          :constructorType="'ganttChart'"
+          class="hc"
+          :options="chartOptions"
+          ref="chart"
+        ></highcharts>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Chart } from "highcharts-vue";
+const JDate = require("jalali-date");
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HighChart",
+  components: {
+    highcharts: Chart,
+  },
+  data() {
+    return {
+      chart_data: [],
+      all_tasks: [],
+      chartOptions: {
+        title: {
+          text: "چارت سازمانی تست",
+        },
+        xAxis: {
+          min: Date.UTC(2021, 10, 17),
+          max: Date.UTC(2021, 11, 15),
+          type: "datetime",
+          labels: {
+            formatter: function () {
+              var someDate = new Date(this.value);
+              return someDate.toLocaleDateString("fa-IR");
+            },
+          },
+        },
+        series: [
+          {
+            name: "Project 1",
+            data: [
+              {
+                name: "تسک 1",
+                start: Date.UTC(2021, 11, 2),
+                end: Date.UTC(2021, 11, 5),
+                completed: 0.25,
+              },
+              {
+                name: "تسک 2",
+                start: Date.UTC(2021, 10, 27),
+                end: Date.UTC(2021, 10, 29),
+                completed: {
+                  amount: 0.1,
+                  fill: "#fab",
+                },
+              },
+              {
+                name: "تسک 3",
+                start: Date.UTC(2021, 10, 20),
+                end: Date.UTC(2021, 10, 25),
+                completed: {
+                  amount: 0.1,
+                  fill: "#fa0",
+                },
+              },
+              {
+                name: "تسک 4",
+                start: Date.UTC(2021, 10, 23),
+                end: Date.UTC(2021, 10, 26),
+                completed: 0.6,
+              },
+            ],
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    get_data() {
+      var this_is = this;
+      var axios = require("axios");
+
+      var config = {
+        method: "post",
+        url: "http://itsm-api.u4u.xyz/api/v1/OperatingProgram/Get?id=1",
+        headers: {
+          Authorization:
+            "Bearer 4b6a33352b7a7453583359504758686c57797433714462677a45686e4e786766466146425a6533715576673d",
+        },
+      };
+
+      axios(config)
+        .then(function (response) {
+          this_is.all_tasks = response.data.data.programTasks;
+          this_is.all_tasks.forEach((task) => {
+            let start = task.startDate;
+            let end = task.endDate;
+
+            var y = Number(start.substr(0, 4));
+            var m = Number(start.substr(5, 2));
+            var d = Number(start.substr(8, 2));
+            var some_variable = this_is.convert(JDate.toGregorian(y, m, d));
+            y = Number(some_variable.substr(0, 4));
+            m = Number(some_variable.substr(5, 2));
+            d = Number(some_variable.substr(8, 2));
+            start = Date.UTC(y, m, d);
+
+
+            y = Number(end.substr(0, 4));
+            m = Number(end.substr(5, 2));
+            d = Number(end.substr(8, 2));
+            some_variable = this_is.convert(JDate.toGregorian(y, m, d));
+            y = Number(some_variable.substr(0, 4));
+            m = Number(some_variable.substr(5, 2));
+            d = Number(some_variable.substr(8, 2));
+            end = Date.UTC(y, m, d);
+
+            this_is.chart_data.push({
+              title: task.title,
+              start: start,
+              end: end,
+            });
+          });
+
+          console.log("this_is.all_tasks");
+          console.log(this_is.all_tasks);
+          console.log("this_is.chart_data");
+          console.log(this_is.chart_data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    convert: function (hi) {
+      var date = new Date(hi),
+        mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+        day = ("0" + date.getDate()).slice(-2);
+      return [date.getFullYear(), mnth, day].join(",");
+    },
+  },
+  mounted() {
+    this.get_data();
+  },
+};
+
+const jdate2 = new JDate(1393, 10, 11);
+const jdate4 = new JDate(new Date(2014, 1, 3));
+const jdate = new JDate(new Date()).format("dddd DD MMMM YYYY"); // => default to today
+
+console.log(jdate2.toGregorian);
+console.log(jdate4.format("dddd DD MMMM YYYY"));
+console.log(jdate);
+
+let str_date = "1393/10/11";
+console.log(Number(str_date.substr(0, 4)));
+console.log(str_date.substr(5, 2));
+console.log(str_date.substr(8, 2));
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style >
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import HighchartsVue from 'highcharts-vue'
 
+import Highcharts from "highcharts";
+import Gantt from "highcharts/modules/gantt";
+Gantt(Highcharts);
+Vue.use(HighchartsVue)
+
+Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
